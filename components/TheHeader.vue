35e524f8e7d491efe48a5f9c59a259fb1bf70814
Marco Sandri <marco@MBP-di-Marco.homenet.telecomitalia.it>
2022-10-30 11:15:50 +0100

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header bg-slate-800 columns-3 py-2 px-5">
+    <div class="w-full h-12"><img class="h-full" src="~/assets/img/lego.svg" /></div>
+    <div class="w-full"></div>
+    <div class="w-full"></div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
